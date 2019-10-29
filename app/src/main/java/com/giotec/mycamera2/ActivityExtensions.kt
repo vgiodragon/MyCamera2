5d4894b2a95e8a5239b72ee54fa1aee562481755
vgiovanny <vgiovanny.mondragon@uni.pe>
2019-10-28 23:43:15 -0500

    Initial commit

---
+++ app/src/main/java/com/giotec/mycamera2/ActivityExtensions.kt
@@ -0,0 +1,17 @@
+package com.giotec.mycamera2
+
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * This file illustrates Kotlin's Extension Functions by extending FragmentActivity.
+ */
+
+/**
+ * Shows a [Toast] on the UI thread.
+ *
+ * @param text The message to show
+ */
+fun FragmentActivity.showToast(text: String) {
+    runOnUiThread { Toast.makeText(this, text, Toast.LENGTH_SHORT).show() }
+}
---
+++ app/src/main/java/com/giotec/mycamera2/CameraActivity.kt
@@ -0,0 +1,19 @@
+package com.giotec.mycamera2
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_camera.*
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CameraActivityFragment.newInstance())
+            .commit()
+    }
+
+}
